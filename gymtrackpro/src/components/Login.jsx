8ccdba2d1313e809d7a9063989ad4897731e1e2d
eliseu daniel <eliseudaniel29@gmail.com>
2024-08-04 23:22:40 -0300

    edit css login screen

--- gymtrackpro/src/components/Login.jsx
+++ gymtrackpro/src/components/Login.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
-import '../App.css'
+
 
 export function Login() {
     return (
-        <div className="Login">
-            <header className="App-header">
-                <div className="App-Form">
-                    <form>
-                        <h3>Bem-Vindo</h3>
-                        <div className="form-input">
-                            <input id="email" className="email" type="email" placeholder="E-mail" />
-                        </div>
-                        <div className="form-input">
-                            <input id="password" className="password" type="password" placeholder="Senha" />
-                        </div>
-                        <button className="btnSub">Entrar</button>
-                        <ul>
-                            <a href="./signup">Cadastre-se</a>
+        <div className="flex justify-center items-center h-screen relative bg-slate-900">
+            <header className="bg-slate-700 rounded-xl w-80">
+                <div className='flex justify-center flex-col gap-2 m-4 '>
+                    <h3 className='flex justify-center text-white text-3xl'>Bem-Vindo</h3>
+                    <div className="flex flex-col gap-4 ">
+                        <input className="rounded-md p-1 email" id="email" type="email" placeholder="E-mail" />
+                        <input className="rounded-md p-1 password" id="password" type="password" placeholder="Senha" />
+                    </div>
+                    <div className='flex justify-center '>
+                        <button className=" rounded-md bg-zinc-600 hover:bg-cyan-500 p-2 text-cyan-300">Entrar</button>
+                    </div>
+                    <div className='flex justify-center'>
+                        <ul >
+                            <a className='p-1 rounded-md text-cyan-600 hover:bg-cyan-500' href="./signup">Cadastre-se</a>
                         </ul>
-                    </form>
+                    </div>
                 </div>
             </header>
         </div>
--- gymtrackpro/src/components/home.jsx
+++ gymtrackpro/src/components/home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../output.css"
+
 
 export function Home() {
     return (
