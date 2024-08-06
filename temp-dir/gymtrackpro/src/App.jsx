bfebd108744e963ae54284d50250fdc1b47bd3a9
eliseu daniel <eliseudaniel29@gmail.com>
2024-08-06 17:23:36 -0300

    Copied files from main to gh-pages

---
+++ temp-dir/gymtrackpro/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Login } from './components/Login'
+import { Signup } from './components/signup'
+import { Home } from './components/home'
+
+function App() {
+  return (
+    <div className='App'>
+      <Login />
+      {/* <Signup /> */}
+      {/* <Home /> */}
+    </div>
+  )
+}
+
+export default App
